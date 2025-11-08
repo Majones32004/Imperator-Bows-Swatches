6c7da1f2e46a6f3edbeebbeddb5424389707f174
Majones32004 <majones32004@gmail.com>
2025-11-08 07:42:33 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,22 @@
-const CACHE="imperator-swatches-v20";
-const ASSETS=["./","./index.html","./style.css","./icons/medallion.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+/* Medallion cache bump patch */
+const CACHE = "imperator-swatches-v30";  // bump
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./woods.json",
+  "./manifest.webmanifest",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/medallion-v2.png",
+  "./sample_aquila.jpg"
+];
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
