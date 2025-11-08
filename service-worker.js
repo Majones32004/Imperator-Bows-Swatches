2a7f2b577f42a3aa6abbce276757fc35cc0a449e
Majones32004 <majones32004@gmail.com>
2025-11-08 08:17:31 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
-/* Wix theme patch cache bump */
-const CACHE = "imperator-swatches-v41";
+/* Wix+ theme cache bump */
+const CACHE = "imperator-swatches-v45";
 self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./woods.json","./icons/icon-192.png","./icons/icon-512.png","./sample_aquila.jpg","./assets/hero-wave.svg","./service-worker.js"])))
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+    "./","./index.html","./style.css","./app.js","./woods.json","./icons/icon-192.png","./icons/icon-512.png","./sample_aquila.jpg","./assets/hero-wave.svg","./service-worker.js"
+  ])));
 });
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
